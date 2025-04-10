7b9eae152760bb17799b00be23d36e3380d8c5f9
Gregory Kelly <git@gregk.ca>
2025-04-10 00:00:38 -0400

    Add TalonSRX for crusher

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,10 +1,8 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 
 public class RobotMap {
 
@@ -13,10 +11,14 @@ public class RobotMap {
     private static final int RIGHT_DRIVE_1 = 1;
     private static final int RIGHT_DRIVE_2 = 2;
 
+    private static final int CRUSHER_MOTOR = 5;
+
     public static final BaseMotorController leftDrive1;
     public static final BaseMotorController leftDrive2;
     public static final BaseMotorController rightDrive1;
     public static final BaseMotorController rightDrive2;
+
+    public static final TalonSRX crusherMotor;
 
     static {
         leftDrive1 = new VictorSPX(LEFT_DRIVE_1);
@@ -26,5 +28,7 @@ public class RobotMap {
         rightDrive1 = new VictorSPX(RIGHT_DRIVE_1);
         rightDrive2 = new VictorSPX(RIGHT_DRIVE_2);
         rightDrive2.follow(rightDrive1);
+
+        crusherMotor = new TalonSRX(CRUSHER_MOTOR);
     }
 }
